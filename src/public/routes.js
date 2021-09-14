90d9625b50de605a65064211bf0cc9a4126e3605
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-09-14 01:54:12 -0400

    new routes added (customers, taxes, products, prices, currencies)

--- src/public/routes.js
+++ src/public/routes.js
@@ -2,6 +2,12 @@
 
 let routes = [
     {path: "/", component: () => import("./components/home.js") },
+    {path: "/taxes", component: () => import("./components/taxes.js") },
+    {path: "/clients", component: () => import("./components/clients.js") },
+    {path: "/currencies", component: () => import("./components/currencies.js") },
+    {path: "/products", component: () => import("./components/products.js") },
+    {path: "/prices", component: () => import("./components/prices.js") },
+
 ];
 
 // exportando rutas designadas
