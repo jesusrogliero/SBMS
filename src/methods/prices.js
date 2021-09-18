1a9f5f330bd7b9bac1b8f5faf0c32e1c9c21015c
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-09-18 12:20:14 -0400

    renamed from ProductPrice to Price

--- src/methods/prices.js
+++ src/methods/prices.js
@@ -54,7 +54,10 @@ const prices = {
             return {message: "Agregado con exito", code: 1};
             
         } catch (error) {
-            return { message: error.message, code:0 };
+			if( !empty( error.errors ) )
+				return {message: error.errors[0].message, code: 0};
+			else
+				return { message: error.message, code: 0 };
         }
     },
 
