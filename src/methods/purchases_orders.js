75632f372d31c583a6b96d0b49de78a4ee6bc827
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-09-18 12:23:32 -0400

    index method fixed, sending unnecessary properties

--- src/methods/purchases_orders.js
+++ src/methods/purchases_orders.js
@@ -1,6 +1,11 @@
 'use strict'
 
+const sequelize = require('sequelize');
+const empty = require('../helpers/empty.js');
 const PurchaseOrder = require('../models/PurchaseOrder.js');
+const Currency = require('../models/Currency.js');
+const Provider = require('../models/Provider.js');
+const PurchaseOrderState = require('../models/PurchaseOrderState.js');
 
 const purchase_orders = {
 
@@ -11,8 +16,38 @@ const purchase_orders = {
 	 */
 	'index-purchases': async function() {
 		try {
-			return await PurchaseOrder.findAll({raw:true});
+			let a =  await PurchaseOrder.findAll({
+				attributes: {
+					include: [
+						[sequelize.col('currency.name'), 'currency_name'],
+						sequelize.col('purchase_orders_state.state'),
+						sequelize.col('provider.full_name'),
+						[sequelize.col('currency.symbol'), 'currency_symbol'],
+					]
+				},
+				include: [
+					{
+						model: Currency,
+						required: true,
+						attributes: []
+					},
+					{
+						model: Provider,
+						required: true,
+						attributes: []
+					},
+					{
+						model: PurchaseOrderState,
+						required: true,
+						attributes: []
+					},
+				],
+				raw:true
+			});
+			console.log(a);
+			return a;
 		} catch (error) {
+	
 			return { message: error.message, code:0} ;
 		}
 	},
@@ -30,9 +65,10 @@ const purchase_orders = {
 
             // creo una nueva compra
             const new_order =  await PurchaseOrder.create({
-                state_id: params.state_id,
                 observation: params.observation,
                 date: params.date,
+				tax: params.tax,
+				state_id: 1,
                 provider_id: params.provider_id,
                 currency_id: params.currency_id,
             });
@@ -40,7 +76,10 @@ const purchase_orders = {
             return {message: "Agregado con exito", code: 1};
             
         } catch (error) {
-            return { message: error.message, code:0 };
+			if( !empty( error.errors ) )
+				return {message: error.errors[0].message, code: 0};
+			else
+				return { message: error.message, code: 0 };
         }
     },
 
@@ -55,7 +94,7 @@ const purchase_orders = {
 		try {
 			let order = await ProductPrice.findByPk(id, {raw: true});
 
-			if(order === null) throw new Error("Esta orden de compra no existe");
+			if( empty(order) ) throw new Error("Esta orden de compra no existe");
 
 			return order;
 
@@ -92,7 +131,7 @@ const purchase_orders = {
 		try {
 			let order = await PurchaseOrder.findByPk(id);
 
-			if(order === null) throw new Error("Esta compra no existe");
+			if( empty(order) ) throw new Error("Esta compra no existe");
 
             if(order.state_id != 1) throw new Error("Esta orden ya fue procesada");
 
