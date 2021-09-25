8e7eb8ecbde2a6f81443cb1262276bd6b6005547
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-09-25 13:14:55 -0400

    method that adjusts the costs,  the one that generates the costs and  the one that shows them were corrected

--- src/methods/products_costs.js
+++ src/methods/products_costs.js
@@ -3,6 +3,9 @@
 const ProductCost = require('../models/ProductCost.js');
 const Currency = require('../models/Currency.js');
 const empty = require('../helpers/empty.js');
+const Product = require('../models/Product.js');
+const Op = require('sequelize');
+const sequelize = require('sequelize');
 
 const products_costs = {
 
@@ -13,7 +16,27 @@ const products_costs = {
 	 */
 	'index-costs': async function() {
 		try {
-			return await ProductCost.findAll({raw:true});
+			return await ProductCost.findAll({
+                attributes: {
+                    include: [
+                        [sequelize.col('currency.symbol'), 'currency_symbol'],
+                        [sequelize.col('product.name'), 'product_name'],
+                    ]
+                },
+                include: [
+                    {
+                        model: Product,
+                        required: true,
+                        attributes: []
+                    },
+                    {
+                        model: Currency,
+                        required: true,
+                        attributes: []
+                    }
+                ],
+                raw:true
+            });
 		} catch (error) {
 			return { message: error.message, code:0} ;
 		}
@@ -27,20 +50,40 @@ const products_costs = {
      * @param {Json} params 
      * @returns message
      */
-	 'generate_product_cost': async function(params) {
+	 'generate_product_cost': async function(order, items) {
         try {
 
             // busco todas las monedas
-            const currencies = Currency.findAll();
+            const currencies =  await Currency.findAll();
+            const currency_order = await Currency.findByPk(order.currency_id); 
 
-            // creo un nuevo costo de producto
-            const new_cost =  await ProductCost.create({
-                productId: params.productId,
-                currencyId: params.currencyId,
-                cost: params.cost
+            items.forEach( async (item) => {
+
+                currencies.forEach( async (currency) => {
+                    let exchange_rate = currency_order.exchange_rate / currency.exchange_rate;
+                    
+                    let products_costs = await ProductCost.findOne({
+                        where:{
+                            product_id: item.product_id,
+                            currency_id: currency.id
+                        }
+                    });
+
+                    // si el costo no existe se genera uno nuevo
+                    if( empty(products_costs) ) {
+                        products_costs = ProductCost.build({
+                            currency_id: currency.id,
+                            product_id: item.product_id,
+                        });
+                    }
+
+                    products_costs.cost = item.price * exchange_rate;
+                    await products_costs.save();
+                });
             });
 
-            return {message: "Agregado con exito", code: 1};
+            return true;
+
         } catch (error) {
             if( !empty( error.errors ) )
                 return {message: error.errors[0].message, code: 0};
@@ -48,6 +91,72 @@ const products_costs = {
                 return { message: error.message, code: 0 };
         }
     },
+
+
+    /**
+     * Metodo que muestra un recurso
+     * 
+     * @param {Json} params 
+     * @returns cost
+     */
+	 'show-cost': async function(id) {
+        try {
+            const cost = await ProductCost.findByPk(id, {raw: true});
+
+            if( empty(cost) ) throw new Error('Este costo no existe');
+
+            return cost;
+
+        } catch (error) {
+            return { message: error.message, code: 0 };
+        }
+    },
+
+
+    /**
+     * Metodo que ajusta los costos
+     * 
+     */
+	 'adjust-costs': async function(currency_id) {
+        try {
+            const currency = await Currency.findByPk(currency_id);
+            const currency_default = await Currency.findOne({
+                where: {
+                    exchange_rate: 1
+                }
+            });
+
+
+            const products_costs = await ProductCost.findAll({
+                where: {
+                    currency_id: currency.id
+                },
+                raw: true   
+            });
+
+
+            let prd_cost = null;
+            for (let i = 0; i < products_costs.length; i++) {
+                
+                prd_cost = await ProductCost.findOne({
+                    where: {
+                        currency_id: currency_default.id,
+                        product_id: products_costs[i].product_id
+                    }
+                });
+
+                prd_cost.cost = currency.exchange_rate * products_costs[i].cost;
+                await prd_cost.save();
+            }
+
+            return {message: 'Costos actualizados correctamente', code:1 };
+
+        } catch (error) {
+            console.log(error);
+            return { message: error.message, code: 0 };
+        }
+    },
+
 
 };
 
