7b9e439548729b75553512817410a49aa14e776f
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-09-30 21:11:35 -0400

    the possibility of adding costs manually is enabled again

--- src/public/components/costs.js
+++ src/public/components/costs.js
@@ -1,18 +1,29 @@
 'use strict'
 
+import('../utils/autocomplete.js');
+
 // componente home
 let costs = Vue.component('costs', {
 
   data: () => ({
+    dialog: false,
+    dialogDelete: false,
     page: 1,
     pageCount: 1,
     search: "",
     hidden: false,
     headers: [
+      {
+        text: 'ID',
+        align: 'start',
+        sortable: false,
+        value: 'id',
+      },
       { text: 'Producto', value: 'product_name' },
       { text: 'Costo', value: 'cost' },
       { text: 'Creado', value: 'createdAt' },
       { text: 'Actualizado', value: 'updatedAt' },
+      { text: 'Acciones', value: 'actions', sortable: false },
     ],
     costs: [],
     editedIndex: -1,
@@ -21,13 +32,23 @@ let costs = Vue.component('costs', {
 
   computed: {
     formTitle () {
-      return this.editedIndex === -1 ? '' : '';
+      return this.editedIndex === -1 ? 'Nuevo Costo' : 'Actualizar un Costo';
+    },
+  },
+
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
     },
   },
 
   created () {
-    this.cleanForm();
     this.initialize();
+    this.cleanForm();
+
   },
 
   methods: {
@@ -38,15 +59,87 @@ let costs = Vue.component('costs', {
 
     cleanForm: function() {
       this.editedItem = {
-          id: '',
-          cost: '',
-        }
+        id: '',
+        product_id: null,
+        currency_id: null,
+        cost: 0
+      }
     },
 
-    format: function(value) {
-      return formatMoney(value);
+    getSelectProduct: function(value) {
+      this.editedItem.product_id = value;
     },
 
+    getSelectCurrency: function(value) {
+      this.editedItem.currency_id = value;
+    },
+    
+    editItem: async function(item) {
+      this.editedIndex = item.id
+      this.editedItem = await execute('show-cost', this.editedIndex);
+      this.dialog = true
+    },
+
+    deleteItem: async function(item) {
+      this.editedIndex = item.id;
+      this.editedItem = await execute('show-cost', this.editedIndex);
+
+      if(this.editedItem.code == 0){
+        alertApp({color:"error", text: this.editedItem, icon: "alert" });
+        this.cleanForm();
+      }
+        
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm: async function() {
+      let result = await execute('destroy-cost', this.editedIndex);
+
+
+      if(result.code == 1) {
+        alertApp({color:"success", text: result, icon: "check" }); 
+      }else{
+        alertApp({color:"error", text: result, icon: "alert" }); 
+      }
+
+      this.closeDelete();
+    },
+
+    close () {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.initialize();
+        this.cleanForm();
+        this.editedIndex = -1;
+      })
+    },
+
+    closeDelete () {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.initialize();
+        this.cleanForm();
+        this.editedIndex = -1;
+      })
+    },
+
+    save: async function() {
+      let result = null;
+      
+      if (this.editedIndex > -1) {
+        result = await execute('update-cost', this.editedItem);
+      } else {
+        result = await execute('create-cost', this.editedItem);
+      }
+
+      if(result.code === 1) {
+        alertApp({color:"success", text: result, icon: "check" }); 
+        this.close();
+      }else{
+        alertApp({color:"error", text: result, icon: "alert" }); 
+      }
+       
+    },
   },
 
 
@@ -87,10 +180,36 @@ let costs = Vue.component('costs', {
       <v-spacer></v-spacer>
       
       <v-dialog
-        :v-model="false"
+        v-model="dialog"
         max-width="500px"
       >
         <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="primary"
+            icon
+            class="mb-2"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon
+            >
+              mdi-plus
+            </v-icon>
+            </v-btn> 
+
+          <v-btn
+            color="primary"
+            icon
+            class="mb-2"
+            v-bind="attrs"
+            @click="initialize"
+          >
+            <v-icon
+            >
+            mdi-reload
+            </v-icon>
+          </v-btn> 
+
 
           <v-btn
             color="primary"
@@ -105,6 +224,81 @@ let costs = Vue.component('costs', {
             </v-icon>
           </v-btn> 
         </template>
+        
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">{{ formTitle }}</span>
+          </v-card-title>
+
+          <v-card-text>
+            <v-container>
+              <v-row>
+              
+              
+                <v-col cols = "12">
+                  <autocomplete-form
+                      uri = "index-products"
+                      label = "Selecciona un Producto"
+                      column = "name"
+                      itemValue = "id"
+                      :defaultValue = "editedItem.product_id"
+                      :getSelect = "getSelectProduct"
+                  />
+               </v-col>  
+               
+               
+               <v-col cols = "12">
+                <autocomplete-form
+                    uri = "index-currencies"
+                    label = "Selecciona una Moneda"
+                    column = "name"
+                    itemValue = "id"
+                    :defaultValue = "editedItem.currency_id"
+                    :getSelect = "getSelectCurrency"
+                />
+                </v-col>  
+
+
+                <v-col cols="12" >
+                  <v-text-field
+                    v-model="editedItem.cost"
+                    label="Ingresa el Costo"
+                  ></v-text-field>
+                </v-col>
+
+              </v-row>
+            </v-container>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="error"
+              text
+              @click="close"
+            >
+              Cancelar
+            </v-btn>
+            <v-btn
+              color="success"
+              text
+              @click="save"
+            >
+              Guardar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="dialogDelete" max-width="600px">
+        <v-card>
+          <v-card-title class="text-h5">Estas seguro que deseas eliminar este Precio?</v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="error" text @click="closeDelete">Cancelar</v-btn>
+            <v-btn color="success" text @click="deleteItemConfirm">Confirmar</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
       </v-dialog>
     </v-toolbar>
   </template>
@@ -118,10 +312,13 @@ let costs = Vue.component('costs', {
     >
       mdi-pencil
     </v-icon>
-  </template>
-
-  <template v-slot:item.cost="{ item }">
-    {{format(item.cost) + ' ' + item.currency_symbol}}
+    <v-icon
+      dense
+      @click="deleteItem(item)"
+      color="error"
+    >
+      mdi-delete
+    </v-icon>
   </template>
 
 </v-data-table>
