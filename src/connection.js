81a68ca0d009a543e47e78bf1e04c0a93175eed4
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-09-30 21:00:18 -0400

    an attempt to create seeds in the application

--- src/connection.js
+++ src/connection.js
@@ -7,9 +7,66 @@ const sequelize = new Sequelize({
 });
 
 
+
+const seeds =  async function(Model, data) {
+	try {
+		for (let i = 0; i < data.length; i++) {
+			await Model.findOrCreate({
+				where: data[i]
+			});
+		}
+		
+	} catch (error) {
+		console.log(error);
+	}
+	
+  };
+
+
+
 (async () => {
-  await sequelize.sync();
+	await sequelize.sync();
+
+
+	await seeds(require('./models/Currency.js'), [
+		{
+			name: 'Bolivares',
+			symbol: 'Bs.S',
+			exchange_rate: 1
+		},
+
+	]).then(e => console.log);
+
+
+  	await seeds(require('./models/PurchaseOrderState.js'), [
+		{state: 'Pendiente'},
+		{state: 'Generada'},
+		{state: 'Aprobada'},
+	]).then(e => console.log);
+
+
+	await seeds(require('./models/ProductType.js'), [
+		{type: 'Unico'},
+		{type: 'Combo'}
+	]).then(e => console.log);
+
+	await seeds(require('./models/Tax.js'), [
+		{
+			name: "Exento",
+			percentage: 0
+		},
+	]).then(e => console.log);
+
+	await seeds(require('./models/InvoiceState.js'), [
+		{state: 'Pendiente'},
+		{state: 'Generada'},
+		{state: 'Aprobada'},
+	]).then(e => console.log);
+
 })();
+
+
+
 
 
 module.exports = sequelize;
