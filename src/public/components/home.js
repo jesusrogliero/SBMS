da75d06b574a618de7177343ece7216b35f1aec2
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-10-01 03:40:14 -0400

    price query widget implemented

--- src/public/components/home.js
+++ src/public/components/home.js
@@ -10,7 +10,10 @@ let home = Vue.component('home', {
 				exchange_rate: 0,
 				sales_today: null,
 				sales_week: null,
-				currency_default: null
+				currency_default: "",
+				products_prices: [],
+				product_id: null,
+				currency_id: 1,
 			}
 	},
 
@@ -20,6 +23,14 @@ let home = Vue.component('home', {
 		this.exchange_rate = currency.exchange_rate;
 	},
 
+	watch: {
+		product_id: function() {
+			this.getProductPrice();
+		},
+		currency_id: function() {
+			this.getProductPrice();
+		}
+	},
 
 	methods: {
 
@@ -28,6 +39,10 @@ let home = Vue.component('home', {
 				this.exchange_rate = parseFloat(val.split(' ').join('').split(',').join('').split('Bs.S').join(''));
 				console.log(this.exchange_rate);
 			}
+		},
+
+		format_money: function(price, symbol) {
+			return `${formatMoney(price)} ${symbol}`;
 		},
 
 		adjust_exchange: async function() {
@@ -52,7 +67,30 @@ let home = Vue.component('home', {
 
 		},
 
-		get_product_costs: async function() {
+		getSelectProduct: async function(val) {
+			this.product_id = val;
+		},
+
+		getSelectCurrency: async function(val) {
+			this.currency_id = val;
+		},
+
+		getProductPrice: async function() {
+
+			try {
+				let result = await execute('get_product_prices', {
+					id: this.product_id,
+					currency_id: this.currency_id
+				});
+				
+				if(result.code === 0) 
+					throw new Error(result.message);
+
+				this.products_prices = result;
+
+			} catch (error) {
+				alertApp({color:"error", text: error, icon: "alert" }); 
+			} 
 
 		},
 
@@ -141,6 +179,93 @@ let home = Vue.component('home', {
 			</v-card>
 			</v-col>
 
+			</v-row>
+
+
+			<v-row>
+				<v-col>
+					<v-card 
+					class="mx-left"
+					
+					elevation="3"
+					color="indigo lighten-5"
+					>
+						<v-card-text>
+						<p class="text-h4 text--primary">
+							Consultar Precio
+						</p>
+				
+							<v-container>
+								<v-row>
+									<v-col cols = "5">
+										<autocomplete-form
+											uri = "index-products"
+											label = "Selecciona el Producto"
+											column = "name"
+											itemValue = "id"
+											:defaultValue = "product_id"
+											:getSelect = "getSelectProduct"
+										/>
+									</v-col>
+
+									<v-col cols = "5">
+									<autocomplete-form
+										uri = "index-currencies"
+										label = "Selecciona la moneda (opcional)"
+										column = "name"
+										itemValue = "id"
+										:defaultValue = "currency_id"
+										:getSelect = "getSelectCurrency"
+									/>
+								</v-col>
+
+								<v-col cols = "2">
+									<v-btn
+										class="ml-2 mt-2"
+										small
+										fab
+										color="primary"
+										@click="getProductPrice"
+									>
+										<v-icon dark>
+										mdi-magnify
+										</v-icon>
+									</v-btn>
+								</v-col>
+								</v-row>
+							</v-container>
+
+							<v-container class="ml-5" >
+								<v-row>
+									
+								<v-card
+									tile
+									width="1000"
+									elevation="0"
+									color="indigo lighten-5"
+							  	>
+
+								  <v-row>
+								  <v-col
+								  	cols="6" sm="4"
+									v-for="(item, i) in products_prices"
+									:key="i"
+								  >
+									<p class="title">Precio: {{item.price_name}}</p>
+									<p class="text-h5">{{ format_money(item.price, item.currency_symbol) }}</p>
+								  
+								  </v-col>
+							  </v-row>
+
+								
+							
+							  </v-card>
+									
+								</v-row>
+						</v-container>
+						</v-card-text>
+					</v-card>
+				</v-col>
 			</v-row>
 		</v-container>
 
