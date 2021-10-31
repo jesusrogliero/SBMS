3f9fd36821417ba18ab13616d94e414319e64e4d
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-10-30 22:59:36 -0400

    Withdrawn the default prices

--- src/methods/prices.js
+++ src/methods/prices.js
@@ -17,8 +17,7 @@ const prices = {
 			return await Price.findAll({
 				attributes: { 
 					include: [ 
-						Price.sequelize.literal("(price*100) || '%' AS price"),
-						Price.sequelize.literal("IIF(is_default, 'SI', 'NO') AS is_default")
+						Price.sequelize.literal("(price*100) || '%' AS price")
 				 	],
 				},
 				raw:true
@@ -41,16 +40,12 @@ const prices = {
 	 'create-price': async function(params) {
         try {
 
-			let price_default = await Price.findOne({ where: { is_default: true } });
-
-			if( !empty(price_default) && params.is_default == true)
-				throw new Error('Ya existe una precio predeterminado');
 
             // creo un nuevo precio
             const new_price =  await Price.create({
                 name: params.name,
                 price: params.price / 100,
-                is_default: params.is_default
+                is_default: false
             });
 
             return {message: "Agregado con exito", code: 1};
@@ -106,26 +101,15 @@ const prices = {
 		try {
 
             if(params.price <= 0) throw new Error('Debes ingresar un precio mayor a 0');
-
-			if( empty(params.is_default) ) throw new Error("Debes ingresar si es o no el precio predeterminado");
 			if( empty(params.name) ) throw new Error("El nombre del precio es obligatorio");
 			if( empty(params.price) ) throw new Error("EL porcentage del precio es obligatorio");
 
 			let price = await Price.findByPk(params.id);
-            let price_default = await Price.findOne({ where: { is_default: true } });
 			
 			if( price === null) throw new Error("Este precio no existe");
 
 			price.name = params.name;
 			price.price = params.price / 100;
-
-            if( price.id != price_default.id) {
-                
-                if(params.is_default === true) {
-                    price_default.is_default = false;
-                    price.is_default = true;
-                }
-            }
 
 			await price.save();
 
@@ -150,9 +134,6 @@ const prices = {
 			let price = await Price.findByPk(id);
 
 			if(price === null) throw new Error("Este precio no existe");
-
-			if(price.is_default === true)
-				throw new Error('No es posible eliminar el precio predeterminado');
 
 			price.destroy();
 
